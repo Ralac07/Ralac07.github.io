61ab043ff49338b42376f7093caf4a3c11e92210
Ryan Larsen <ryanlarsen.dev@gmail.com>
2025-10-18 11:33:04 +1100

    add WIP message to pdf compressor

--- src/routes/CompressPDF/index.tsx
+++ src/routes/CompressPDF/index.tsx
@@ -17,6 +17,7 @@ export default function PDFCompressor() {
   }
   return (
     <>
+      <h3>WIP</h3>
       <FilePicker
         supportedMimes={["application/pdf"]}
         uploadCallback={uploadCallback}
